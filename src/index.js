c23d51eda042cf82db90217538568d6c7de5b5ef
awatersny <awatersny@gmail.com>
2025-03-12 14:01:39 -0400

    Take guesses from user

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
+const getRand = num => Math.floor(Math.random() * num + 1)
+const rangePrompt = "Please enter a number from 10 to 100 or click cancel to quit:"
+const setGuessPrompt = num => `Please guess a number from 1 to ${num} or click cancel to change the range:`
 let rangeInput = ""
+let guessInput = ""
+let outOf = 0
+let guess = 0
+let target = 0
+
 do {
-  let outOf = 0
-  rangeInput = prompt("Please enter a number from 10 to 100:")
+  rangeInput = prompt(rangePrompt)
   if(rangeInput !== null) {
     outOf = parseInt(rangeInput)
-    if(outOf > 9 && outOf < 101) {
-  
+    if(outOf >= 10 && outOf <= 100) {
+      target = getRand(outOf)
+      
+      while(guessInput !== null) {
+        guessInput = prompt(setGuessPrompt(outOf))
+        guess = parseInt(guessInput)
+        if(guess >= 1 && guess <= outOf) {
+          alert(guess)
+        } else {
+          alert("Your guess input is invalid.  Please try again.")
+        }
+      }
+
     } else {
-      alert("Invalid Input")
+      alert("Your input is invalid.  Please try again.")
     }
   }
 } while (rangeInput !== null);
