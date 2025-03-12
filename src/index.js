f66dfb5b49841f8ac32f0dc5dba3cd74830ca3b1
awatersny <awatersny@gmail.com>
2025-03-12 14:02:48 -0400

    Fixed bug where user would get an invalid input prompt when exiting guess screen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
+const setGuessPrompt = num => `Please guess a number from 1 to ${num} or click cancel to change the range:`
 const getRand = num => Math.floor(Math.random() * num + 1)
 const rangePrompt = "Please enter a number from 10 to 100 or click cancel to quit:"
-const setGuessPrompt = num => `Please guess a number from 1 to ${num} or click cancel to change the range:`
 let rangeInput = ""
 let guessInput = ""
-let outOf = 0
-let guess = 0
+let chances = 0
 let target = 0
+let guess = 0
+let outOf = 0
 
 do {
   rangeInput = prompt(rangePrompt)
@@ -14,15 +15,19 @@ do {
     if(outOf >= 10 && outOf <= 100) {
       target = getRand(outOf)
       
-      while(guessInput !== null) {
+      do {
         guessInput = prompt(setGuessPrompt(outOf))
         guess = parseInt(guessInput)
         if(guess >= 1 && guess <= outOf) {
-          alert(guess)
+          alert(`Guess: ${guess} Target: ${target}`)
         } else {
-          alert("Your guess input is invalid.  Please try again.")
+          if(guessInput !== null){
+            alert("Your guess input is invalid.  Please try again.")
+          } else {
+            alert("Going back...")
+          }
         }
-      }
+      } while(guessInput !== null)
 
     } else {
       alert("Your input is invalid.  Please try again.")
