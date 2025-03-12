311945ca03589befa885c16c8c3ec232cca94410
awatersny <awatersny@gmail.com>
2025-03-12 17:21:04 -0400

    Create a start button

--- src/index.js
+++ src/index.js
@@ -10,7 +10,33 @@ let target = 0
 let guess = 0
 let outOf = 0
 
-play()
+const body = document.querySelector("body")
+
+const main = document.createElement("main")
+main.style.display = "flex"
+main.style.alignItems = "center"
+main.style.justifyContent = "center"
+main.style.height = "500px"
+
+const btn = document.createElement("button")
+btn.textContent = "Start"
+btn.style.color = "white"
+btn.style.backgroundColor = "blue"
+btn.style.border = "10px outset skyblue"
+btn.style.height = "80px"
+btn.style.width = "150px"
+
+body.appendChild(main)
+main.appendChild(btn)
+
+btn.addEventListener("click", () => {
+  console.log("clicked")
+  btn.style.border = "10px inset skyblue"
+  setTimeout(() => {
+    btn.style.border = "10px outset skyblue"
+    play()
+  }, "0");
+})
 
 function play() {
   do {
